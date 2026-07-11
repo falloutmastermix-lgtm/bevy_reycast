fc03573a2536056f8ea889bf988f052c0e7d5213
fallout <47340012+kpk-i710@users.noreply.github.com>
2026-07-12 00:51:11 +0500

    теперь есть колеса и тело одно

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use bevy_xpbd_2d::prelude::*;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        
         .add_plugins(PhysicsPlugins::default())
         .add_plugins(PhysicsDebugPlugin::default())
         .insert_resource(Gravity(Vec2::new(0.0, -9.81)))
@@ -16,42 +17,26 @@ fn main() {
 struct CarMotor;
 
 fn setup(mut commands: Commands) {
+    // ВАЖНО: Добавляем камеру, иначе ничего не увидите
     commands.spawn(Camera2dBundle::default());
 
     // 1. Пол
     commands.spawn((
         RigidBody::Static,
-        Collider::cuboid(2000.0, 50.0),
+        Collider::rectangle(2000.0, 50.0), // Исправил cuboid на rectangle по предупреждению
         TransformBundle::from(Transform::from_xyz(0.0, -250.0, 0.0)),
     ));
 
-    // 2. Кузов (центр машины)
-    let body_entity = commands.spawn((
+    // 2. Составное тело (кузов + 2 колеса)
+    commands.spawn((
         RigidBody::Dynamic,
-        // Коллайдер меньше спрайта, чтобы колеса не "взрывали" его
-        Collider::rectangle(80.0, 30.0),
+        Collider::compound(vec![
+            (Vec2::ZERO, 0.0, Collider::rectangle(100.0, 40.0)),
+            (Vec2::new(-50.0, -40.0), 0.0, Collider::circle(15.0)),
+            (Vec2::new(50.0, -40.0), 0.0, Collider::circle(15.0)),
+        ]),
         TransformBundle::from(Transform::from_xyz(0.0, 0.0, 0.0)),
-    )).id();
-
-    // 3. Колеса
-    let wheel_offsets = [Vec2::new(-50.0, -40.0), Vec2::new(50.0, -40.0)];
-
-    for offset in wheel_offsets {
-        let wheel = commands.spawn((
-            RigidBody::Dynamic,
-            Collider::circle(15.0), // Колеса чуть меньше
-            ExternalTorque::default(),
-            CarMotor,
-            TransformBundle::from(Transform::from_xyz(offset.x, offset.y, 0.0)),
-        )).id();
-
-        // Шарнир: привязка к кузову
-        let joint = RevoluteJoint::new(body_entity, wheel)
-            .with_local_anchor_1(offset)
-            .with_local_anchor_2(Vec2::ZERO);
-        
-        commands.entity(wheel).insert(joint);
-    }
+    ));
 }
 
 fn drive_system(
